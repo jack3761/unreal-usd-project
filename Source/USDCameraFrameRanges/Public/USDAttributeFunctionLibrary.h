82870bb08605041215834bddc7333ecfcb58718a
Jack Purkiss <jp829@exeter.ac.uk>
2024-07-09 16:15:15 +0100

    working template with float, double and int functions

--- Source/USDCameraFrameRanges/Public/USDAttributeFunctionLibrary.h
+++ Source/USDCameraFrameRanges/Public/USDAttributeFunctionLibrary.h
@@ -23,23 +23,9 @@ class USDCAMERAFRAMERANGES_API UUSDAttributeFunctionLibrary : public UBlueprintF
 	GENERATED_BODY()
 	
 public:
-	// Templated function for internal use
-	template <typename T>
-	static T GetUsdAttributeInternal(AUsdStageActor* StageActor, FString PrimName, FString AttrName);
-
-	// BlueprintCallable functions
-	UFUNCTION(BlueprintCallable, Category = "USD Attributes")
-	static float GetUsdFloatAttribute(AUsdStageActor* StageActor, FString PrimName, FString AttrName);
-
-	UFUNCTION(BlueprintCallable, Category = "USD Attributes")
-	static int GetUsdIntAttribute(AUsdStageActor* StageActor, FString PrimName, FString AttrName);
-
-	// UFUNCTION(BlueprintCallable, Category = "USD Attributes")
-	// static double GetUsdDoubleAttribute(AUsdStageActor* StageActor, FString PrimName, FString AttrName);
-
+	
 
 private:
-	static void GetSdfPathWithName(UE::FUsdPrim& CurrentPrim, FString TargetName, UE::FSdfPath& OutPath);
 
 	
 };
